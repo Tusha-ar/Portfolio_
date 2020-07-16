57692f8fce6fb895c48b1b228ae8bd3866295f60
Tushar <tushargpt224@gmail.com>
2020-07-17 01:03:05 +0530

    initial commit

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './work.scss';
 import ProjectCard from '../ProjectCard/ProjectCard';
-import gsap from 'gsap/gsap-core';
-import { CSSRulePlugin } from 'gsap/all';
+// import gsap from 'gsap/gsap-core';
+// import { CSSRulePlugin } from 'gsap/all';
 import Aos from 'aos';
 import 'aos/dist/aos.css'
 
@@ -11,11 +11,11 @@ import 'aos/dist/aos.css'
 const Work = ()=>{
     useState(()=>{
         Aos.init({once: true, duration: 1000})
-        gsap.registerPlugin(CSSRulePlugin)
-        gsap.from('.title', 3, {
-            scale: 0.2,
-            color: 'red'
-        })
+        // gsap.registerPlugin(CSSRulePlugin)
+        // gsap.from('.title', 3, {
+        //     scale: 0.2,
+        //     color: 'red'
+        // })
     })
     return(
         <div className='work'>
@@ -23,7 +23,7 @@ const Work = ()=>{
             <div className='projects'>
                 <ProjectCard img={require('../../Assets/Projects/filter.png')} title= 'Filter out'/>
                 <ProjectCard img={require('../../Assets/Projects/mukul.png')} title= "mukul's portfolio"/>
-                <ProjectCard img= 'https://raw.githubusercontent.com/Tusha-ar/Portfolio_new/master/src/Asstets/project4.jpg' title= 'News'/>
+                <ProjectCard img= {require('../../Assets/Projects/news.jpg')} title= 'News'/>
                 <ProjectCard img={require('../../Assets/Projects/red_eyed.png')} title= 'Red eyed'/>
                 <ProjectCard img={require('../../Assets/Projects/stocks.png')} title= 'stocks'/>            
                 <ProjectCard img='https://raw.githubusercontent.com/Tusha-ar/Travel-Planner/master/ss/Screenshot%20(4).png' title= 'travel planner'/>            
